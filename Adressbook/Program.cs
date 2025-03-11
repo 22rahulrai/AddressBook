fd526d1e6708654a2039ad7193d0ec58a712f84d
Rahul Rai <rairayrahul22@gmail.com>
2025-03-11 10:23:35 +0530

    uc-1

--- Adressbook/Program.cs
+++ Adressbook/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        static List<Contact> listcontacts = new List<Contact>;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Contact newcontact = new Contact("rahul", "rai", "123  ", "Panchkula", "HR", "62701", "217 - 555 - 1234", "john@gmai.com");
+            listcontacts.Add(newcontact);
+            Console.WriteLine("new contact added");
         }
     }
 }
+
+//Ability to create a Contacts in Address
+//Book with first and last names, address,
+//city, state, zip, phone number and
+//email…
+//- Program is written using IDE like Visual Studio
+//- Every UC is in a separate Git Branch and then merged with main
+//- Naming Convention, Indentation, etc Code Hygiene will be checked during
+//Review
+//- Git Check In Comments and Version History will be monitored
